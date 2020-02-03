c6868ecc6c46ae33aec6ef6a6400f2a9d4b95d26
league <league@imac-10.attlocal.net>
2020-02-02 17:29:03 -0800

    File Recorder Done

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,26 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a message: ");
+		String hey = scan.nextLine();
+		
+		try {
+			FileWriter fw = new FileWriter("src/_00_Intro_To_File_Input_and_Output/test3.txt");
+			
+			fw.write(hey);
+				
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
